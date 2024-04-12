31f8099078d4666ad10ac54a2372dd5753c197bc
Ryichi <richard4pech@gmail.com>
2024-04-12 17:14:08 +1000

    Create working build for rendering a graphic window

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,45 @@
 
 using namespace std;
 
-int main() {
-    if (SDL_Init(SDL_INIT_EVERYTHING) > 0) {
-        cout << "SDL_Init failed with error:" << SDL_GetError() << endl;
+int main(int argc, char *argv[]) {
+
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow("Physics Engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 500, 500, SDL_WINDOW_ALWAYS_ON_TOP);
+
+    if (window == NULL) {
+        cout << "Could not create SDL window: " << SDL_GetError() << endl;
         return EXIT_FAILURE;
     }
 
-    string hi = "hi";
-    cout << hi << endl;
+    SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, 0);
 
+    SDL_RenderClear(renderer);
+
+    SDL_Rect rect;
+    rect.x = 250;
+    rect.y = 150;
+    rect.w = 200;
+    rect.h = 200;
+
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+    SDL_RenderDrawRect(renderer, &rect);
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+
+    SDL_RenderPresent(renderer);
+
+    SDL_Event windowEvent;
+
+    while (true) {
+        if (SDL_PollEvent(&windowEvent)) {
+            if (SDL_QUIT == windowEvent.type) {
+                break;
+            }
+        }
+    }
+
+    SDL_DestroyWindow(window);
     SDL_Quit();
 
     return EXIT_SUCCESS;
